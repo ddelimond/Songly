ff14592f5af494396070d708030cfa615133f04a
Darren Delimond <darrendelimond@gmail.com>
2023-01-20 23:19:17 -0500

    =starting artist details

--- src/components/SongBar.jsx
+++ src/components/SongBar.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 import PlayPause from './PlayPause';
 
 const SongBar = ({ song, i, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
-  <div className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${activeSong?.title === song?.title ? 'bg-[#4c426e]' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
+  <div className={`w-full flex flex-row items-center hover:bg-[#d6c9b1]/40 ${activeSong?.title === song?.title ? 'bg-[#d6c9b1]/40' : 'bg-transparent'} py-2 p-4 rounded-lg cursor-pointer mb-2`}>
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
       <img
         className="w-20 h-20 rounded-lg"
-        src={artistId ? song?.attributes?.artwork?.url.replace('{w}', '125').replace('{h}', '125') : song?.images?.coverart}
+        src={artistId ? song?.attributes?.artwork?.url.replace('{w}', '125').replace('{h}', '125') : song?.images?.coverart || '/src/images/album_art_itunes_by_stainless2_d3kxnbe.png'}
         alt={song?.title}
       />
       <div className="flex-1 flex flex-col justify-center mx-3">
