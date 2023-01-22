65452c8b8652f5c283edccc9149ada50a7cacddb
Darren Delimond <darrendelimond@gmail.com>
2023-01-22 10:15:33 -0500

    =done

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -21,7 +21,7 @@ const Discover = () => {
         <div className=" flex flex-col">
             <div className="w-full flex justify-between items-center
             sm:flex-row flex-col mt-4 mb-10">
-                <h2 className=' font-bold text-3xl text-left text-[#20615B]'>Discover {genreListId || genreTitle}</h2>
+                <h2 className=' font-bold text-3xl text-left text-[#20615B]'>Discover {genreListId.split('_').join(' ') || genreTitle}</h2>
                 <select
                     onChange={(e) => dispatch(selectGenreListId(e.target.value))}
                     value={genreListId || 'POP'}
